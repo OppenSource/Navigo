74f03d042d3e92796664a71edd46879cf25ce3ef
OppenSource <brice.nanyang@institutsaintjean.org>
2023-11-12 23:38:45 +0100

    Design ui et validations sur formulaire

--- src/app/views/common/forgot-password/forgot-password.page.ts
+++ src/app/views/common/forgot-password/forgot-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
+  forgot: FormGroup | any;
+  account: any = {
+    account: '',
+  };
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private toastController: ToastController,
+    public navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.forgot = this.formBuilder.group({
+      account: ['', [Validators.required]],
+    });
   }
 
+  get errorControl() {
+    return this.forgot.controls;
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Renseignez tous les champs avant de continuer.',
+      duration: 5000,
+      color: 'danger',
+      icon: 'alert-circle-outline',
+      position: 'top',
+    });
+    await toast.present();
+  }
+
+  async successLogin() {
+    const toast = await this.toastController.create({
+      message:
+        'Mot de passe réinitialisé avec succès, veillez consulter votre messagérie téléphonique.',
+      duration: 4000,
+      color: 'success',
+      icon: 'checkmark-done-circle-outline',
+      position: 'top',
+    });
+    await toast.present();
+  }
+
+  submitForm = () => {
+    if (this.forgot.valid) {
+      this.account.account = this.forgot.value.account;
+      setTimeout(() => {
+        this.successLogin();
+        this.navCtrl.navigateForward('login');
+      }, 1000);
+
+      return false;
+    } else {
+      this.presentToast();
+      return console.log('');
+    }
+  };
 }
