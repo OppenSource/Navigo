b9bddc5d4aac85aaf980624d9b8579f301bf4062
OppenSource <brice.nanyang@institutsaintjean.org>
2023-11-12 13:19:28 +0100

    Design ui et validations sur formulaire

--- src/app/views/common/login/login.page.ts
+++ src/app/views/common/login/login.page.ts
@@ -8,15 +8,56 @@ import { NavController, ToastController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  ionicForm: FormGroup | any;
-  account: any =  {
+  login: FormGroup | any;
+  account: any = {
     username: '',
-    password: ''
+    password: '',
   };
 
-  constructor( public formBuilder: FormBuilder, private toastController: ToastController, public navCtrl  : NavController) { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private toastController: ToastController,
+    public navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.login = this.formBuilder.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.maxLength(20)]],
+    });
   }
 
+  get errorControl() {
+    return this.login.controls;
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Renseignez tous les champs avant de continuer.',
+      duration: 5000,
+      color: 'danger',
+      icon: 'alert-circle-outline',
+      position: 'top',
+    });
+    await toast.present();
+  }
+
+  submitForm = () => {
+    if (this.login.valid) {
+      this.account.username = this.login.value.username;
+      this.account.password = this.login.value.password;
+      console.log(this.account);
+      return false;
+    } else {
+      this.presentToast();
+      return console.log('');
+    }
+  };
 }
